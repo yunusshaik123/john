25810c0b92864b625b4acdd8b2a56a8635d3c25e
yunus <yunus@LAPTOP-HLOMKQ7I>
2023-05-30 17:29:20 +0530

    run

---
+++ hibernate/src/main/java/hibernate/Test.java
@@ -0,0 +1,42 @@
+package hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+       Configuration cfg = new Configuration();
+		
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		Transaction ts = session.beginTransaction();
+		
+		Customer cobj = new Customer();
+		
+		cobj.setOrdername("yunus");
+		cobj.setLocation("Pulivendula");
+		
+		session.persist(cobj);
+		
+		ts.commit();
+		
+		session.close();
+		
+		System.out.println("Done.");
+
+		
+
+		
+
+	}
+
+}
